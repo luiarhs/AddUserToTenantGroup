768f6c43041941661cfc1f5dc3d5304c36696b16
Luis Angel <luiarhs@yandex.com>
2020-12-09 22:46:46 -0600

    [add] function and dependencies

--- src/main/java/com/function/Function.java
+++ src/main/java/com/function/Function.java
@@ -1,43 +1,80 @@
 package com.function;
 
 import com.microsoft.azure.functions.ExecutionContext;
-import com.microsoft.azure.functions.HttpMethod;
-import com.microsoft.azure.functions.HttpRequestMessage;
-import com.microsoft.azure.functions.HttpResponseMessage;
-import com.microsoft.azure.functions.HttpStatus;
-import com.microsoft.azure.functions.annotation.AuthorizationLevel;
 import com.microsoft.azure.functions.annotation.FunctionName;
-import com.microsoft.azure.functions.annotation.HttpTrigger;
+import com.microsoft.azure.functions.annotation.QueueTrigger;
 
-import java.util.Optional;
+import com.microsoft.graph.auth.confidentialClient.ClientCredentialProvider;
+import com.microsoft.graph.auth.enums.NationalCloud;
+import com.microsoft.graph.models.extensions.DirectoryObject;
+import com.microsoft.graph.models.extensions.IGraphServiceClient;
+import com.microsoft.graph.requests.extensions.GraphServiceClient;
+
+import com.google.gson.GsonBuilder;
+
+import java.util.List;
 
 /**
- * Azure Functions with HTTP Trigger.
+ * Azure Functions with Azure Storage Queue trigger.
  */
 public class Function {
-    /**
-     * This function listens at endpoint "/api/HttpExample". Two ways to invoke it using "curl" command in bash:
-     * 1. curl -d "HTTP Body" {your host}/api/HttpExample
-     * 2. curl "{your host}/api/HttpExample?name=HTTP%20Query"
-     */
-    @FunctionName("HttpExample")
-    public HttpResponseMessage run(
-            @HttpTrigger(
-                name = "req",
-                methods = {HttpMethod.GET, HttpMethod.POST},
-                authLevel = AuthorizationLevel.ANONYMOUS)
-                HttpRequestMessage<Optional<String>> request,
-            final ExecutionContext context) {
-        context.getLogger().info("Java HTTP trigger processed a request.");
 
-        // Parse query parameter
-        final String query = request.getQueryParameters().get("name");
-        final String name = request.getBody().orElse(query);
+    ClientCredentialProvider authProvider = new ClientCredentialProvider(System.getenv("clientId"),
+            List.of(System.getenv("scope")), System.getenv("secret"), System.getenv("tenantId"), NationalCloud.Global);
 
-        if (name == null) {
-            return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body("Please pass a name on the query string or in the request body").build();
-        } else {
-            return request.createResponseBuilder(HttpStatus.OK).body("Hello, " + name).build();
+    IGraphServiceClient graphClient = GraphServiceClient.builder().authenticationProvider(authProvider).buildClient();
+
+    @FunctionName("AddUserToGroup")
+    public void run(
+        @QueueTrigger(name = "message", queueName = "b2cgroupassign", connection = "AzureWebJobsStorage") String message,
+        final ExecutionContext context) {
+
+        context.getLogger().info("Function initialized");
+
+
+        var builder = new GsonBuilder();
+        builder.setPrettyPrinting();
+
+        var gson = builder.create();
+
+        final var userId = gson.fromJson(message, Message.class).getUserId();
+        final var groupId = gson.fromJson(message, Message.class).getGroupId();
+
+        if (userId == null)
+            context.getLogger().warning("Null value for userId");
+        else if (graphClient == null)
+            context.getLogger().warning("Unable to initialize graph client");
+        else {
+            context.getLogger().info("Processing user with userId " + userId);
+
+            var directoryObject = new DirectoryObject();
+            directoryObject.id = userId;
+
+            var post = graphClient.groups(groupId).members().references().buildRequest()
+                    .post(directoryObject);
+
+            context.getLogger().info("Graph client response: " + post.toString());
         }
+    }
+}
+
+class Message {
+    private String userId;
+    private String groupId;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String id) {
+        this.userId = id;
+    }
+
+    public String getGroupId() {
+        return groupId;
+    }
+
+    public void setGroupId(String id) {
+        this.groupId = id;
     }
 }
